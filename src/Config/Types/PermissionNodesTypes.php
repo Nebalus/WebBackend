e4969e20afe7a53d8e6c2807c9c1e21955a9948a
Nebalus <56486387+Nebalus@users.noreply.github.com>
2025-08-08 10:41:58 +0200

    Update PermissionNodesTypes.php

--- src/Config/Types/PermissionNodesTypes.php
+++ src/Config/Types/PermissionNodesTypes.php
@@ -14,11 +14,22 @@ class PermissionNodesTypes
     public const string FEATURE_REFERRAL = 'feature.referral';
     public const string FEATURE_REFERRAL_OWN = 'feature.referral.own';
     public const string FEATURE_REFERRAL_OWN_CREATE = 'feature.referral.own.create';
-    public const string FEATURE_REFERRAL_OWN_CREATE_LIMIT = 'feature.referral.own.view.limit';
+    public const string FEATURE_REFERRAL_OWN_CREATE_LIMIT = 'feature.referral.own.create.limit';
     public const string FEATURE_REFERRAL_OWN_DELETE = 'feature.referral.own.delete';
     public const string FEATURE_REFERRAL_OWN_EDIT = 'feature.referral.own.edit';
     public const string FEATURE_REFERRAL_OWN_ENABLED = 'feature.referral.own.enabled';
     public const string FEATURE_REFERRAL_OTHER = 'feature.referral.other';
     public const string FEATURE_REFERRAL_OTHER_DELETE = 'feature.referral.other.delete';
     public const string FEATURE_REFERRAL_OTHER_EDIT = 'feature.referral.other.edit';
+
+    public const string FEATURE_BLOG = 'feature.blog';
+    public const string FEATURE_BLOG_OWN = 'feature.blog.own';
+    public const string FEATURE_BLOG_OWN_CREATE = 'feature.blog.own.create';
+    public const string FEATURE_BLOG_OWN_CREATE_LIMIT = 'feature.blog.own.create.limit';
+    public const string FEATURE_BLOG_OWN_DELETE = 'feature.blog.own.delete';
+    public const string FEATURE_BLOG_OWN_EDIT = 'feature.blog.own.edit';
+    public const string FEATURE_BLOG_OWN_ENABLED = 'feature.blog.own.enabled';
+    public const string FEATURE_BLOG_OTHER = 'feature.blog.other';
+    public const string FEATURE_BLOG_OTHER_DELETE = 'feature.blog.other.delete';
+    public const string FEATURE_BLOG_OTHER_EDIT = 'feature.blog.other.edit';
 }
