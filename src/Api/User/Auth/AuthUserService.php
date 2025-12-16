1e168fa9cf122b0bd64e6af9c19f17b1ae209229
nebalus <coding@nebalus.dev>
2025-12-16 14:49:01 +0100

    feature-NWS-4: Added Email Templates

--- src/Api/User/Auth/AuthUserService.php
+++ src/Api/User/Auth/AuthUserService.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace Nebalus\Webapi\Api\User\Auth;
 
+use Exception;
 use Fig\Http\Message\StatusCodeInterface;
 use Nebalus\Webapi\Config\GeneralConfig;
 use Nebalus\Webapi\Exception\ApiException;
+use Nebalus\Webapi\Factory\TwigFactory;
 use Nebalus\Webapi\Repository\UserRepository\MySqlUserRepository;
 use Nebalus\Webapi\Slim\ResultInterface;
+use Nebalus\Webapi\Utils\IpUtils;
 use Nebalus\Webapi\Value\Result\Result;
 use ReallySimpleJWT\Exception\BuildException;
 use ReallySimpleJWT\Token;
 use Resend\Client as ResendClient;
+use Twig\Environment as TwigEnvironment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 readonly class AuthUserService
 {
@@ -21,6 +28,8 @@ readonly class AuthUserService
         private GeneralConfig $generalConfig,
         private AuthUserResponder $responder,
         private ResendClient $resendClient,
+        private TwigEnvironment $twig,
+        private IpUtils $ipUtils,
     ) {
     }
 
@@ -37,11 +46,23 @@ readonly class AuthUserService
 
         $expirationTime = time() + $this->generalConfig->getJwtExpirationTime();
 
+        $rendered = "...";
+        $currentDateTime = new \DateTimeImmutable();
+
+        try {
+            $rendered = $this->twig->render("/email/on_login.twig", [
+                "username" => $user->getUsername()->asString(),
+                "ip_address" => $this->ipUtils->getClientIP(),
+                "login_time" => $currentDateTime->format("Y-m-d H:i:s"),
+            ]);
+        } catch (Exception $e) {
+        }
+
         $this->resendClient->emails->send([
             'from' => 'noreply@nebalus.dev',
-            'to' => 'contact@nebalus.dev',
-            'subject' => 'Hello Und Willkommen',
-            'html' => '<strong>...</strong>',
+            'to' => $user->getEmail()->asString(),
+            'subject' => 'Login Confirmation',
+            'html' => '<strong>' . $rendered . '</strong>',
         ]);
 
         $token = Token::builder($this->generalConfig->getJwtSecret())
--- src/Factory/TwigFactory.php
+++ src/Factory/TwigFactory.php
@@ -2,14 +2,22 @@
 
 namespace Nebalus\Webapi\Factory;
 
+use Twig\Environment as Twig;
+use Twig\Loader\FilesystemLoader;
+
 readonly class TwigFactory
 {
-    public function __construct(
-    ) {
+    public function __construct()
+    {
     }
 
-    public function __invoke(): void
+    public function __invoke(): Twig
     {
-       
+        $loader = new FilesystemLoader(__DIR__ . '/../../templates');
+        return new Twig($loader, [
+            'cache' => false,
+            'strict_variables' => true,
+            'autoescape' => 'html',
+        ]);
     }
 }
