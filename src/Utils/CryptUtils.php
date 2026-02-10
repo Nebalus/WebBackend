196bb5f0e349f6ea4a62e75dadab0670291f4afa
Nebalus <56486387+Nebalus@users.noreply.github.com>
2026-02-10 11:48:48 +0100

    Add generateAnonymousId method to CryptUtils

--- src/Utils/CryptUtils.php
+++ src/Utils/CryptUtils.php
@@ -1,1 +1,14 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Nebalus\WebAPI\S;
+
+class CryptUtils
+{
+    public function generateAnonymousId(string $ipAddress, string $userAgent): string
+    {
+        $dailySalt = date('Y-m-d');
+        return hash('sha256', $ipAddress . $userAgent . $dailySalt);
+    }
+}
