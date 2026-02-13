c7c50dcf04a9066c2505141a768cd6d3853b4ff5
nebalus <coding@nebalus.dev>
2026-02-13 23:34:50 +0100

    feature-NWS-4: Hotfix

--- src/Repository/ReferralRepository/MySqlReferralRepository.php
+++ src/Repository/ReferralRepository/MySqlReferralRepository.php
@@ -50,6 +50,7 @@ readonly class MySqlReferralRepository
             VALUES (:referral_id, :anonymous_identity_hash)
         SQL;
 
+        echo $anonymousIdentityHash->asString();
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':referral_id', $referralId->asInt());
         $stmt->bindValue(':anonymous_identity_hash', $anonymousIdentityHash->asString());
